16d3dd0c1b8af1ada541050c674cba2c8dc21f5e
KO4UYJ <99303085+firealarmss@users.noreply.github.com>
2023-07-11 05:34:07 -0500

    Update receiver.rs

--- src/commands/receiver.rs
+++ src/commands/receiver.rs
@@ -86,36 +86,63 @@ impl Receiver {
 
 let sub_tx = tx.clone();
 let mut first_packet_received = false;
-thread::spawn(move || loop {
+let mut previous_audio_end = 0;
+
+thread::spawn(move || {
+    let socket = UdpSocket::bind("0.0.0.0:5000").expect("Failed to bind UDP socket");
     let mut buffer = [0u8; 8192];
-       match socket.recv_from(&mut buffer) {
+    let mut audio_buffer = Vec::new();
+
+    loop {
+        match socket.recv_from(&mut buffer) {
             Ok((packet_size, _)) => {
                 if packet_size >= 4 {
                     let src_id = u16::from_be_bytes([buffer[packet_size - 4], buffer[packet_size - 3]]);
                     let dst_id = u16::from_be_bytes([buffer[packet_size - 2], buffer[packet_size - 1]]);
                     let audio_data = &buffer[..(packet_size - 4)];
-
-                    if first_packet_received {
-                        println!(
-                            "[INFO] RECEIVED FIRST PACKET: (length: {}, src_id: {}, dst_id: {})",
-                            packet_size,
-                            src_id,
-                            dst_id
-                        );
-                        first_packet_received = true;
-                    }
+                    println!(
+                        "[INFO] RECEIVED FIRST PACKET: (length: {}, src_id: {}, dst_id: {})",
+                        packet_size,
+                        src_id,
+                        dst_id
+                    );
+                    first_packet_received = true;
 
                     if audio_data.len() == 320 {
-                        let audio = Vec::from(audio_data);
-                        if sub_tx.send(Some(audio)).is_err() {
-                            return;
+                        // Append the received audio to the buffer
+                        audio_buffer.extend_from_slice(audio_data);
+
+                        // Check if enough audio samples are accumulated for playback
+                        while audio_buffer.len() >= 320 {
+                            // Extract a chunk of 320 samples for playback
+                            let audio_chunk = audio_buffer.drain(..320).collect::<Vec<u8>>();
+
+                            // Calculate the time offset for smooth playback
+                            let current_audio_end = previous_audio_end + 320;
+
+                            // Simulate the playback delay if necessary
+                            let now = time::Instant::now();
+                            let expected_playback_time = time::Duration::from_micros(current_audio_end as u64);
+                            if now < expected_playback_time {
+                                thread::sleep(expected_playback_time - now);
+                            }
+
+                            // Send the audio chunk for playback
+                            if sub_tx.send(Some(audio_chunk)).is_err() {
+                                return;
+                            }
+
+                            // Update the previous audio end position
+                            previous_audio_end = current_audio_end;
                         }
                     }
                 }
             }
             Err(_) => return,
         }
-    });
+    }
+});
+
 
         Self {
             discord_channel,
