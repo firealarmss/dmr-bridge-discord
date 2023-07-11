98092410917010afc81828302cbb1529916a23a7
KO4UYJ <99303085+firealarmss@users.noreply.github.com>
2023-07-11 07:06:59 -0500

    Update receiver.rs

--- src/commands/receiver.rs
+++ src/commands/receiver.rs
@@ -190,8 +190,13 @@ impl Receiver {
     }
 }
 
-fn send_discord_webhook(src_id: &str, dst_id: &str) {
-    let webhook_url = "https://discord.com/api/webhooks/1128288295068110909/VlmFL3xYoQW5cpv_Otuxn2D1hgD3N-V_0TTp4lt2Z8OUaI0Zvi9ElwMCsW2-u0Oq4ya4";
+async fn some_function() -> Result<(), Box<dyn std::error::Error>> {
+    let request = client.post("https://discord.com/api/webhooks/1128288295068110909/VlmFL3xYoQW5cpv_Otuxn2D1hgD3N-V_0TTp4lt2Z8OUaI0Zvi9ElwMCsW2-u0Oq4ya4").body("data");
+
+    if let Err(e) = request.send().await {
+        // Handle the error...
+    }
+
     let client = Client::new();
     let payload = format!(
         r#"{{"content": "[INFO] RECEIVED PACKET: (src_id: {}, dst_id: {})"}}"#,
@@ -201,8 +206,5 @@ fn send_discord_webhook(src_id: &str, dst_id: &str) {
         .post(webhook_url)
         .body(payload)
         .header("Content-Type", "application/json");
-
-    if let Err(e) = request.send() {
-        eprintln!("Failed to send Discord webhook: {:?}", e);
-    }
+    Ok(()) 
 }
